bc7feec6e71d5854bf4bee1427f7a6786597ae5f
CodeCoo <jetsada.g@gmail.com>
2016-09-06 09:59:01 +0700

    edit more arrow.js

--- babel/babel-labs/dist/arrow.js
+++ babel/babel-labs/dist/arrow.js
@@ -1,24 +1,39 @@
-"use strict";
+'use strict';
 
-[1, 2, 3, 4].map(function (x) {
+// General 
+
+var func = function func(x) {
     return x * x;
-});
+}; // concise syntax, implied "return"
+var func = function func(x, y) {
+    return x + y;
+}; // with block body, explicit "return" needed
+var func = function func() {
+    return { foo: 1 };
+}; // return object literal
+var func = function func() {
+    return { foo: function foo() {} };
+}; // return method
 
-var odds = [];
+//Loops
 
+var even = [];
 [1, 2, 3, 4, 5, 6].forEach(function (x) {
     if (x % 2 === 0) {
-        odds.push(x);
+        even.push(x);
     }
 });
-
-var odds = [1, 2, 3, 4, 5, 6].map(function (x) {
-    return x % 2;
+console.log('Output #1', even);
+var double = [];
+double = [1, 2, 3, 4, 5, 6].map(function (x) {
+    return x * 2;
 });
+console.log('Output #2', double);
 
+//Class
 var person = {
     _name: "CodeCoo",
-    _friends: [],
+    _friends: ['Lisa', 'Obama'],
     printFriends: function printFriends() {
         var _this = this;
 
@@ -27,5 +42,39 @@ var person = {
         });
     }
 };
-
 person.printFriends();
+
+//With Rest Parameter
+
+var f = function f() {
+    return arguments.length <= 0 ? undefined : arguments[0];
+};
+console.log('Args is ', f(1)); // 1
+
+//More examples
+
+(function () {
+    return "foobar";
+})(); // IIFE, returns "foobar"
+
+var simple = function simple(a) {
+    return a > 15 ? 15 : a;
+};
+simple(16); // 15
+
+var max = function max(a, b) {
+    return a > b ? a : b;
+};
+
+//all array & filtering & Mapping
+
+var arr = [5, 6, 13, 0, 1, 18, 23];
+var sum = arr.reduce(function (a, b) {
+    return a + b;
+}); // 66
+var even = arr.filter(function (v) {
+    return v % 2 == 0;
+}); // [6, 0, 18]
+var double = arr.map(function (v) {
+    return v * 2;
+}); // [10, 12, 26, 0, 2, 36, 46]
--- babel/babel-labs/src/arrow.js
+++ babel/babel-labs/src/arrow.js
@@ -1,23 +1,54 @@
-[1,2,3,4].map(x=>x*x)
 
-var odds=[];
+// General 
 
+var func = x => x * x;                  // concise syntax, implied "return"
+var func = (x, y) => { return x + y; }; // with block body, explicit "return" needed
+var func = () => ({  foo: 1  });         // return object literal
+var func = () => ({foo: function() {} });   // return method
+
+//Loops
+
+var even=[];
 [1,2,3,4,5,6].forEach(x=>{
   if(x%2===0){
-      odds.push(x);
+      even.push(x);
   }
 })
+console.log('Output #1',even);
+var double=[];
+double=[1,2,3,4,5,6].map(x=>x*2);
+console.log('Output #2',double);
 
-var odds=[1,2,3,4,5,6].map(x=>x%2);
-
+//Class
 var person = {
     _name:"CodeCoo",
-    _friends:[],
+    _friends:['Lisa','Obama'],
     printFriends(){
         this._friends.forEach(x=>{
             console.log(this._name+" knows "+x);
         });
+
     }
 }
-
 person.printFriends();
+
+//With Rest Parameter
+
+var f = (...args)=>args[0];
+console.log('Args is ',f(1)); // 1
+
+//More examples
+
+(() => "foobar")() // IIFE, returns "foobar"
+
+var simple = a => a > 15 ? 15 : a; 
+simple(16); // 15
+
+let max = (a, b) => a > b ? a : b;
+
+//all array & filtering & Mapping
+
+var arr = [5, 6, 13, 0, 1, 18, 23];
+var sum = arr.reduce((a, b) => a + b);  // 66
+var even = arr.filter(v => v % 2 == 0); // [6, 0, 18]
+var double = arr.map(v => v * 2);       // [10, 12, 26, 0, 2, 36, 46]
